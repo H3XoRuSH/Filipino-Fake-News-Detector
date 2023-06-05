4380aec3c6396e4999badc14aab764f247472fe8
Gabriel Samonte <samontegabriel2001@gmail.com>
2023-06-05 23:18:26 +0800

    chore: service worker clean up

--- service_worker.js
+++ service_worker.js
@@ -17,20 +17,24 @@ function sendHighlight(info) {
       "Content-Type": "application/json"
     }
   })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.is_fake);
-      let base = "not" ;
-      if (data.is_fake == 1) {
-        base = "fake";
-      }
-      if (data.is_fake == 2) {
-        base = "invalid";
-      }
-      let link = base + ".html";
-      chrome.tabs.create({ url: link });
-    })
-    .catch(error => {
-      console.error("Error:", error);
-    });
+
+  .then(response => response.json())
+
+  .then(data => {
+    console.log(data.is_fake);
+    let base = "not" ;
+    if (data.is_fake == 1) {
+      base = "fake";
+    }
+    if (data.is_fake == 2) {
+      base = "invalid";
+    }
+    let link = base + ".html";
+    chrome.tabs.create({ url: link });
+  })
+
+  .catch(error => {
+    console.error("Error:", error);
+  });
+  
 }
